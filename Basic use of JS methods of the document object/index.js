9032b2ae9ab8fac6a7d713f3e89fa4806546d004
Primalpancake92 <johnle.jl39@gmail.com>
2025-02-01 09:32:01 +1100

    Some basic methods of the JS DOM.

---
+++ Basic use of JS methods of the document object/index.js
@@ -0,0 +1,11 @@
+// This is the line in the console of node
+console.log("This is the something website.");
+
+// This is the windows alert method
+window.alert("THIS IS THE SOMETHING WEBSITE.");
+
+// document.getElementById().textContent is used to change text content
+document.getElementById("heading1").textContent = "Suck my dick ";
+
+// We can do this for the paragraph that was made earlier
+document.getElementById("paragraph1").textContent = "There is more text coming our way.";
